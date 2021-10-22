76fa8d8c87a7f78fa39b957e20d3fe432ca84679
Haroun Ach <harounacho21@gmail.com>
2021-10-22 14:19:18 +0100

    add and test orderSlice

---
+++ state/features/order/orderSlice.js
@@ -0,0 +1,41 @@
+import { createSlice, createSelector } from "@reduxjs/toolkit";
+
+const initialState = {
+  shipping: {
+    fullName: "",
+    address: "",
+    city: "",
+    postalCode: "",
+    country: "",
+  },
+  billing: {
+    fullName: "",
+    address: "",
+    city: "",
+    postalCode: "",
+    country: "",
+  },
+  paymentMethod: "creditCard",
+};
+
+const orderSlice = createSlice({
+  name: "order",
+  initialState: initialState,
+  reducers: {
+    addShipping(state, action) {
+      state.shipping = action.payload;
+    },
+    addBilling(state, action) {
+      state.billing = action.payload;
+    },
+    addPaymentMethod(state, action) {
+      state.paymentMethod = action.payload;
+    },
+  },
+});
+
+// action creators
+export const { addShipping, addBilling, addPaymentMethod } = orderSlice.actions;
+
+// reducer
+export default orderSlice.reducer;
