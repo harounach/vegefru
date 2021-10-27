0c3ae7c8817ebd0e2fd540dff9b90b3e6316a81b
Haroun Ach <harounacho21@gmail.com>
2021-10-27 16:26:07 +0100

    Create product model

--- models/product/productModel.js
+++ models/product/productModel.js
@@ -1,1 +1,32 @@
-// TODO: Define product model
+import mongoose, { Schema, model } from "mongoose";
+
+const reviewSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    qty_unit: { type: String, required: true },
+    qty: { type: Number, required: true, default: 1 },
+    countInStock: { type: Number, required: true },
+    rating: { type: Number, required: true },
+    numReviews: { type: Number, required: true },
+    reviews: [reviewSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ProductModel = mongoose.models.User || model("Product", productSchema);
+
+export default ProductModel;
